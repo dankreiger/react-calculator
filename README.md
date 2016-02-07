7298e19fe8ac1ba929bdba0fec838837189638dd
Dan Kreiger <dan@dankreiger.com>
2016-02-07 14:44:25 -0500

    React skeleton with automated shell script setup

---
+++ README.md
@@ -0,0 +1,53 @@
+
+# React Skeleton Quickstart
+
+### Run this from the command line
+<pre><code>
+chmod +x ./auto_setup.sh
+./auto_setup
+</code></pre>
+
+
+# React Skeleton Manual Setup
+<pre><code>
+npm init
+npm install -g browserify
+npm install --save react
+npm install --save react-dom
+npm install --save babelify
+npm install --save watchify
+npm install --save babel-preset-react
+cat package.json
+mkdir -p src/components
+mkdir -p public/js
+touch public/index.html
+touch src/main.jsx
+touch public/js/main.js
+</code></pre>
+
+## Important (npm server will not start unless this change is made):
+### in <code>package.json</code> change:
+
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+### to:
+  "scripts": {
+    "start": "watchify src/main.jsx -v -t [babelify --presets [ react ] ] -o public/js/main.js"
+  },
+
+### run <code>npm start</code>
+
+### create <code>.gitignore</code>
+<pre>
+<code>
+cat > .gitignore << GITIGNORE
+.gitignore
+node_modules
+GITIGNORE
+</code>
+</pre>
+
+
+For the detailed lecture this skeleton is based off of, check out [Mark Price's Udemy Tutorial](https://www.udemy.com/react-flux/learn)
+
---
+++ auto_setup.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+git init
+
+cat > .gitignore << EOF
+.gitignore
+node_modules
+EOF
+
+
+FILL_IN=$(printf "\n\n \xF0\x9F\x90\xB6 ${GREEN}Fill in app info:${NC}")
+
+echo $FILL_IN
+
+npm init
+npm install -g browserify
+
+npmmodules=('react' 'react-dom' 'babelify' 'watchify' 'babel-preset-react')
+
+
+for i in "${npmmodules[@]}";do npm install --save $i; done
+
+
+mkdir -p src/components
+mkdir -p public/js
+touch public/index.html
+touch src/main.jsx
+touch public/js/main.js
+
+
+
+
+cat > src/main.jsx << EOF
+var React = require('react');
+var ReactDom = require('react-dom');
+var List = require('./components/List.jsx');
+ReactDom.render(<List />, document.getElementById('ingredients'));
+EOF
+
+
+
+cat > src/components/List.jsx << EOF
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+var ingredients = [{"id":1, "text":"ham"}, {"id":2, "text":"cheese"}, {"id":3, "text":"potatoes"}];
+var List = React.createClass({
+  render: function(){
+    var listItems = ingredients.map(function(item) {
+      return <ListItem key={item.id} ingredient={item.text} />;
+    });
+    return (<ul>{listItems}</ul>);
+  }
+});
+module.exports = List;
+EOF
+
+
+cat > src/components/ListItem.jsx << EOF
+var React = require('react');
+var ListItem = React.createClass({
+  render: function() {
+    return(
+      <li>
+        <h4>{this.props.ingredient}</h4>
+      </li>
+    );
+  }
+});
+
+module.exports = ListItem;
+EOF
